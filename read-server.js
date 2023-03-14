b6aa35935a1f01f2e240aa97044e823163c7fd28
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2023-03-14 08:55:10 +0330

    Add js script to read websocket data

---
+++ read-server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const Pusher = require('pusher-js')
+const Echo = require('laravel-echo').default
+
+const options = {
+    broadcaster: 'pusher',
+    key: process.env.PUSHER_APP_KEY,
+    wsHost: process.env.PUSHER_HOST,
+    wsPort: process.env.PUSHER_PORT,
+    forceTLS: process.env.PUSHER_SCHEME == 'https',
+    disableStats: true,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+}
+
+const echo = new Echo({
+    ...options,
+    client: new Pusher(options.key, options),
+})
+
+const channel = echo.channel('webserver')
+
+channel.listen('.log', (data) => {
+    console.info(data)
+})
