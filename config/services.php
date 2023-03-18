26cc493617a330cde24e6570684c31bbe6e4c2b5
mahdi.aslami <mahdi.aslami@partsoftware.com>
2023-03-18 13:43:57 +0330

    Add watch server command configs

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'webserver' => [
+        'log' => env('WEBSERVER_LOG'),
+        'period' => env('WEBSERVER_PERIOD'),
+    ],
 ];
