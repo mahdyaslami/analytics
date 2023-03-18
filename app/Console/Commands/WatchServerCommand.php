c44822014118469151c1ded263a631c4cbe57d56
mahdi.aslami <mahdi.aslami@partsoftware.com>
2023-03-18 15:37:21 +0330

    Complete watch server command

--- app/Console/Commands/WatchServerCommand.php
+++ app/Console/Commands/WatchServerCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Events\WebserverLog;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
 
 class WatchServerCommand extends Command
 {
@@ -11,11 +12,80 @@ class WatchServerCommand extends Command
 
     protected $description = 'Watch the state of server';
 
+    protected $ps = [];
+
     public function handle(): void
     {
-        while (true) {
-            event(new WebserverLog(rand()));
-            sleep(1);
+        $this->init();
+        $this->start();
+
+        while ($this->running()) {
+            $this->tasks();
+            $this->wait();
+        }
+
+        $this->printErrors();
+    }
+
+    private function init()
+    {
+        $this->ps['websockets'] = Process::command(['php', base_path('artisan'), 'websockets:serve']);
+        $this->ps['tail'] = Process::command(['tail', '-f', config('services.webserver.log')]);
+    }
+
+    private function start()
+    {
+        foreach ($this->ps as $title => $process) {
+            $this->info('Run '.ucfirst($title).' process');
+            $this->ps[$title] = $process->start();
+        }
+    }
+
+    public function tasks()
+    {
+        foreach ($this->ps as $title => $process) {
+            $task = $title.'Task';
+            if (method_exists($this, $task)) {
+                $this->{$task}($process);
+            }
+        }
+    }
+
+    private function printErrors()
+    {
+        foreach ($this->ps as $title => $process) {
+            $output = $process->latestOutput();
+            $errorOutput = $process->latestErrorOutput();
+            
+            if (strlen($output) > 0 || strlen($errorOutput) > 0) {
+                $this->error('Errors for '.ucfirst($title).' process:');
+                $this->line($process->latestOutput());
+                $this->line($process->latestErrorOutput());
+            }
+        }
+    }
+
+    private function running(): bool
+    {
+        return array_reduce(
+            $this->ps,
+            fn ($c, $p) => $c && $p->running(),
+            true,
+        );
+    }
+
+    private function wait()
+    {
+        sleep(config('services.webserver.period'));
+    }
+
+    public function tailTask($tail)
+    {
+        $output = $this->ps['tail']->latestOutput();
+
+        if ($len = strlen($output)) {
+            $this->info('Read '.$len.' bytes');
+            event(new WebserverLog($output));
         }
     }
 }
