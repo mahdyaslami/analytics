c143455b7046c163fce63467a5c976ba18c365aa
mahdi.aslami <mahdi.aslami@partsoftware.com>
2023-03-18 16:45:22 +0330

    Add read server command

---
+++ app/Console/Commands/ReadServerCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\WebserverLog;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+
+class ReadServerCommand extends Command
+{
+    protected $signature = 'server:read';
+
+    protected $description = 'Read server logs';
+
+    public function handle(): void
+    {
+        $reader = Process::command(['node', base_path('read-server.js')]);
+        $reader = $reader->start();
+
+        while ($reader->running()) {
+            $output = trim($reader->latestOutput());
+
+            if (strlen($output)) {
+                WebserverLog::parse($output);
+            }
+
+            $this->wait();
+        }
+    }
+
+    private function wait()
+    {
+        sleep(config('services.webserver.period'));
+    }
+}
