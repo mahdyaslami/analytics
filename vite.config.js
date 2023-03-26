d31ebb2390f29ff7f3ac07d45aaab7f5e85975a3
KadSin <kadkhodaeisina@gmail.com>
2023-03-26 09:25:58 +0430

    Allow import vue without extension
    Allow js in jsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -17,4 +18,14 @@ export default defineConfig({
             },
         }),
     ],
-});
+    resolve: {
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, './resources/js') },
+            { find: '@root', replacement: path.resolve(__dirname, './') },
+        ],
+        extensions: ['.vue', '.js', '.json'],
+    },
+    test: {
+        globals: true,
+    },
+})
