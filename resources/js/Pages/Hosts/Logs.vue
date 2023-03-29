7154f4b5c1654f9795023fa84f5b322deed3e61a
KadSin <kadkhodaeisina@gmail.com>
2023-03-29 11:38:26 +0430

    Create a clickable link for the log's request.route

--- resources/js/Pages/Hosts/Logs.vue
+++ resources/js/Pages/Hosts/Logs.vue
@@ -3,8 +3,16 @@ import { Head } from '@inertiajs/vue3'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import BaseTable from '@/Components/BaseTable'
 import Pagination from '@/Components/Pagination'
+import { computed } from 'vue'
 
-defineProps({
+const logsData = computed(
+    () => props.logs.data.map((l) => ({
+        ...l,
+        request: parseRequest(l),
+    }))
+)
+
+const props = defineProps({
     logs: {
         type: Object,
         required: true,
@@ -31,6 +39,16 @@ function formatDate(date) {
         hour12: false,
     })
 }
+
+function parseRequest(log) {
+    const [method, route, version] = log.request.split(' ')
+    return {
+        method,
+        route,
+        version,
+        url: `http://${log.host}${route}`,
+    }
+}
 </script>
 
 <template>
@@ -48,13 +66,25 @@ function formatDate(date) {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg overflow-x-auto">
                     <BaseTable
                         :head="tableHeaders"
-                        :body="logs.data"
+                        :body="logsData"
                         :count="logs.total"
                         :per-page="logs.per_page"
                         countable-descending
                     >
                         <template #time_local="{item}">
                             {{ formatDate(item.time_local) }}
+                        </template>
+
+                        <template #request="{ item }">
+                            {{ item.request.method }}
+                            <a
+                                class="text-blue-600"
+                                target="_blank"
+                                :href="item.request.url"
+                            >
+                                {{ item.request.route }}
+                            </a>
+                            {{ item.request.version }}
                         </template>
                     </BaseTable>
                 </div>
